3c88bf7485933d96ad6d37640eadaabb45ccd0b7
a3888s <sowdirect@gmail.com>
2023-08-23 13:19:45 +0300

    23.08-lesson1-done

---
+++ lesson1.py
@@ -0,0 +1,89 @@
+# Тема: Змінні, Типи даних - Строка.
+
+# Змінні
+# = - оператори присвоєння
+# name = 'itrun'  # створення змінної name
+# print(name)
+# Які змінні можливо створювати
+# 1) Змінні завжди повинні писатись маленькими буквами
+# 2) Змінні повинні завжди починатись з алфавітних символів.
+# Вірно: name, age, name1, name2, number, n1, n2
+# Невірно: !name, 1name, 2name
+# name2 = "itrun"
+# print(name2)
+
+# Як створювати змінні із двох слів:
+# В даному випадку використовується знак "_"
+# Приклад:
+# student_name = "Asel", user_password="sdcfwfd341231"
+# number_1, number_2
+
+# ТЕМА: Строка
+# Строка = це стандартний тип даних в Python
+# Означає текстовий формат даних
+# Будь-який текст та порядок символів, котрі вставлені в "", '' рахується строкою
+
+# first_name = "John"
+# last_name = "Jackson"
+# print("Hello", first_name)
+# # Форматування строк
+# print(f"My name is {first_name}")
+#
+# # Конкатенація - з'єднання строк за допомогою "+"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+# # Приклади конкатенації
+# text = "I am" + " 10 years old"
+# print(text)
+# word = "A" + "p" + "p" + "l" + "e"
+# print(word)
+# Методи строк - це спеціальні команди для строк, які допомагають взаємодіють зі строками
+# та з ними працюють
+
+# name = "asel"
+# print(name.title())
+# title = це метод, який робить перший символ строки великою
+# text = "i am smith"
+# print(text.title())
+# Особливість title - він перетворює всі слова в строці великими
+
+# capitalize - робить тільки перший символ строки великим
+# text = "I am smith"
+# print(text.capitalize())
+
+# lower, upper - методи строк
+# text = "abc"
+# print(text.upper())
+# name = "Anton"
+# print(name.upper())
+# text2 = "aBc24"
+# print(text2.upper())
+# Метод upper - робить всі маленькі алфавітні символи великими
+
+# text = "ABC"
+# print(text.lower())
+# name = "aNtoN"
+# print(name.lower())
+# метод lover - робить всі заглавні букви строки маленькими
+# Сенс методів редагування строк, отримання інформації про строку, взаємодія зі строкою
+
+# Функція input()
+# name = input("Please, enter your name: ")
+# # input() - це системна функція, яка створює можливість вносити інформацію із консолі(терміналу)
+# print(f"Hello, {name}")
+
+# number1 = int(input("Please, enter first number: "))
+# number2 = int(input("Please, enter second number: "))
+# # input виводить всі дані в форматі строки(string)
+# print(number1+number2)
+# * множення, / ділення, різниця
+
+# Метод строк - count()
+# text = "Apple"
+# print(text.count("p"))
+# text2 = "lalala"
+# print(text2.count("la"))
+
+
+
